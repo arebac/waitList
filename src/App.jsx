0d5641f895f2790ff64d3951494a4e441e075dfe
arebac <j.cabreraortiz@outlook.com>
2025-03-01 07:37:52 -0400

    tweaking app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Waitlist from "./components/WaitList";
+import "../src/App.css"
 
 function App() {
   return (
-    <>
+    <div className="fill">
       <Waitlist />
-    </>
+    </div>
   );
 }
 
